9c6206142ef1d8b3af11b5894e97c40e2da732ca
Adrian Irving-Beer <wisq@wisq.net>
2020-01-10 05:57:50 -0500

    Add :weekly support (and update timex).

--- lib/dd_numerics/metrics/common/period.ex
+++ lib/dd_numerics/metrics/common/period.ex
@@ -14,48 +14,98 @@ defmodule DDNumerics.Metrics.Common.Period do
   end
 
   defp parse_period({:daily, time, zone}, max_age, end_datetime) do
-    zoned_datetime = end_datetime |> Timex.Timezone.convert(zone)
-    current_time = zoned_datetime |> DateTime.to_time()
+    zoned_end = end_datetime |> Timex.Timezone.convert(zone)
+    wanted_time = parse_time_of_day(time)
 
-    wanted_time = parse_daily_time(time)
+    case is_too_recent?(zoned_end, wanted_time, max_age) do
+      true -> Timex.shift(zoned_end, days: -1)
+      false -> zoned_end
+    end
+    |> set_time_of_day(wanted_time)
+    |> Timex.Timezone.convert("Etc/UTC")
+  end
 
-    time_in_day =
+  defp parse_period({:weekly, day, time}, max_age, end_time) do
+    parse_period({:weekly, day, time, "Etc/UTC"}, max_age, end_time)
+  end
+
+  defp parse_period({:weekly, day, time, zone}, max_age, end_datetime) do
+    zoned_end = end_datetime |> Timex.Timezone.convert(zone)
+    wanted_day = parse_day_of_week(day)
+    wanted_time = parse_time_of_day(time)
+
+    case is_too_recent?(zoned_end, wanted_time, max_age) do
+      true -> Timex.shift(zoned_end, days: -1)
+      false -> zoned_end
+    end
+    |> rewind_to_day_of_week(wanted_day)
+    |> set_time_of_day(wanted_time)
+    |> Timex.Timezone.convert("Etc/UTC")
+  end
+
+  defp rewind_to_day_of_week(datetime, wanted_day) do
+    current_day = datetime |> DateTime.to_date() |> Date.day_of_week()
+    days_ago = rem(current_day + 7 - wanted_day, 7)
+    Timex.shift(datetime, days: -days_ago)
+  end
+
+  defp is_too_recent?(datetime, cutoff_time, max_age) do
+    Time.diff(datetime, cutoff_time, :second) < max_age
+  end
+
+  # To set the time, we first try to take midnight
+  # and add the corresponding duration.
+  #
+  # This will automatically adjust for any 
+  # timezone shifts, e.g. daylight savings.
+  #
+  # However, we also do a `set` at the end,
+  # to make absolutely sure we get the correct
+  # time, and it hasn't been offset due to DST.
+  defp set_time_of_day(datetime, wanted_time) do
+    from_midnight =
       wanted_time
       |> Time.diff(~T[00:00:00], :microsecond)
       |> Timex.Duration.from_microseconds()
 
-    case Time.diff(current_time, wanted_time, :second) do
-      secs when secs < max_age ->
-        # wanted_time is either in the future (negative),
-        # or within the last max_age seconds.
-        # Use wanted_time from yesterday.
-        Timex.shift(zoned_datetime, days: -1)
-
-      _ ->
-        # wanted_time is sufficiently far in the past.
-        zoned_datetime
-    end
+    datetime
     |> Timex.beginning_of_day()
-    |> Timex.add(time_in_day)
-    |> Timex.Timezone.convert("Etc/UTC")
+    |> Timex.add(from_midnight)
+    |> Timex.set(time: wanted_time, microsecond: {0, 0})
   end
 
-  defp parse_daily_time(time) when is_binary(time) do
+  defp parse_time_of_day(time) when is_binary(time) do
     String.split(time, ":", parts: 3)
     |> Enum.map(&String.to_integer/1)
-    |> parse_daily_time_parts()
+    |> parse_tod_parts()
   end
 
-  defp parse_daily_time(hour) when is_integer(hour) do
-    parse_daily_time_parts([hour, 0, 0])
+  defp parse_time_of_day(hour) when is_integer(hour) do
+    parse_tod_parts([hour, 0, 0])
   end
 
-  defp parse_daily_time_parts([hour, minute, second]) do
+  defp parse_tod_parts([hour, minute]) do
+    parse_tod_parts([hour, minute, 0])
+  end
+
+  defp parse_tod_parts([hour, minute, second]) do
     {:ok, time} = Time.new(hour, minute, second)
     time
   end
 
-  defp parse_daily_time_parts([hour, minute]) do
-    parse_daily_time_parts([hour, minute, 0])
-  end
+  defp parse_day_of_week(:monday), do: 1
+  defp parse_day_of_week(:tuesday), do: 2
+  defp parse_day_of_week(:wednesday), do: 3
+  defp parse_day_of_week(:thursday), do: 4
+  defp parse_day_of_week(:friday), do: 5
+  defp parse_day_of_week(:saturday), do: 6
+  defp parse_day_of_week(:sunday), do: 7
+  defp parse_day_of_week(:mon), do: 1
+  defp parse_day_of_week(:tue), do: 2
+  defp parse_day_of_week(:wed), do: 3
+  defp parse_day_of_week(:thu), do: 4
+  defp parse_day_of_week(:fri), do: 5
+  defp parse_day_of_week(:sat), do: 6
+  defp parse_day_of_week(:sun), do: 7
+  defp parse_day_of_week(num) when is_integer(num) and num in 1..7, do: num
 end
--- test/dd_numerics/metrics/common/period_test.exs
+++ test/dd_numerics/metrics/common/period_test.exs
@@ -68,10 +68,187 @@ defmodule DDNumerics.Metrics.Common.PeriodTest do
              dt(~N[2019-01-19 16:38:48.907577])
            }
 
-    # Winter time, in future:
+    # Winter time, within max_age:
     assert Period.time_range({:daily, "12:23", "America/Toronto"}, 1800, winter) == {
              dt(~N[2019-01-18 17:23:00]),
              dt(~N[2019-01-19 16:38:48.907577])
+           }
+  end
+
+  test "time_range/3 with {:daily, time, zone} operates across DST boundaries" do
+    on_edt_day = dt(~N[2019-03-10 16:38:48.907577])
+    on_est_day = dt(~N[2019-11-03 16:38:48.907577])
+
+    # Today, 09:23 EDT (-4) = 13:23 UTC
+    assert Period.time_range({:daily, "09:23", "America/Toronto"}, 1800, on_edt_day) == {
+             dt(~N[2019-03-10 13:23:00]),
+             dt(~N[2019-03-10 16:38:48.907577])
+           }
+
+    # Today, 00:23 EST (-5) = 05:23 UTC
+    assert Period.time_range({:daily, "00:23", "America/Toronto"}, 1800, on_edt_day) == {
+             dt(~N[2019-03-10 05:23:00]),
+             dt(~N[2019-03-10 16:38:48.907577])
+           }
+
+    # Yesterday, 12:23 EST (-5) = 17:23 UTC
+    assert Period.time_range({:daily, "12:23", "America/Toronto"}, 1800, on_edt_day) == {
+             dt(~N[2019-03-09 17:23:00]),
+             dt(~N[2019-03-10 16:38:48.907577])
+           }
+
+    # Today, 09:23 EST (-5) = 14:23 UTC
+    assert Period.time_range({:daily, "09:23", "America/Toronto"}, 1800, on_est_day) == {
+             dt(~N[2019-11-03 14:23:00]),
+             dt(~N[2019-11-03 16:38:48.907577])
+           }
+
+    # Today, 00:23 EDT (-4) = 04:23 UTC
+    assert Period.time_range({:daily, "00:23", "America/Toronto"}, 1800, on_est_day) == {
+             dt(~N[2019-11-03 04:23:00]),
+             dt(~N[2019-11-03 16:38:48.907577])
+           }
+
+    # Today, 01:23 EDT (-4) = 04:23 UTC
+    # This is during the "lost hour" of daylight savings.
+    # (There's currently no way to specify 01:23 EST.)
+    assert Period.time_range({:daily, "01:23", "America/Toronto"}, 1800, on_est_day) == {
+             dt(~N[2019-11-03 05:23:00]),
+             dt(~N[2019-11-03 16:38:48.907577])
+           }
+
+    # Yesterday, 12:23 EDT (-4) = 16:23 UTC
+    assert Period.time_range({:daily, "12:23", "America/Toronto"}, 1800, on_est_day) == {
+             dt(~N[2019-11-02 16:23:00]),
+             dt(~N[2019-11-03 16:38:48.907577])
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time} rewinds today to `time`" do
+    now = dt(~N[2020-01-10 09:37:33.881594Z])
+
+    assert Period.time_range({:weekly, :tuesday, "03:21"}, 900, now) == {
+             dt(~N[2020-01-07 03:21:00]),
+             now
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time} uses last week if `day` is later in the week" do
+    now = dt(~N[2020-01-10 09:37:33.881594Z])
+
+    assert Period.time_range({:weekly, :sunday, "03:21"}, 900, now) == {
+             dt(~N[2020-01-05 03:21:00]),
+             now
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time} uses this week if `day` is today and `time` is long enough ago" do
+    now = dt(~N[2020-01-10 09:37:33.881594Z])
+
+    assert Period.time_range({:weekly, :friday, "03:21"}, 900, now) == {
+             dt(~N[2020-01-10 03:21:00]),
+             now
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time} uses last week if `day` is today but `time` is too recent" do
+    now = dt(~N[2020-01-10 09:37:33.881594Z])
+
+    assert Period.time_range({:weekly, :friday, "09:35"}, 900, now) == {
+             dt(~N[2020-01-03 09:35:00]),
+             now
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time, zone} operates in TZ `zone`" do
+    summer = dt(~N[2019-07-19 16:38:48.907577])
+    winter = dt(~N[2019-01-19 16:38:48.907577])
+
+    # Summer time, earlier in the week:
+    assert Period.time_range({:weekly, :wed, "09:23", "America/Toronto"}, 1800, summer) == {
+             dt(~N[2019-07-17 13:23:00]),
+             dt(~N[2019-07-19 16:38:48.907577])
+           }
+
+    # Summer time, earlier today:
+    assert Period.time_range({:weekly, :fri, "09:23", "America/Toronto"}, 1800, summer) == {
+             dt(~N[2019-07-19 13:23:00]),
+             dt(~N[2019-07-19 16:38:48.907577])
+           }
+
+    # Summer time, within max_age:
+    assert Period.time_range({:weekly, :fri, "12:23", "America/Toronto"}, 1800, summer) == {
+             dt(~N[2019-07-12 16:23:00]),
+             dt(~N[2019-07-19 16:38:48.907577])
+           }
+
+    # Winter time, earlier today:
+    assert Period.time_range({:weekly, :sat, "09:23", "America/Toronto"}, 1800, winter) == {
+             dt(~N[2019-01-19 14:23:00]),
+             dt(~N[2019-01-19 16:38:48.907577])
+           }
+
+    # Winter time, within max_age:
+    assert Period.time_range({:weekly, :sat, "12:23", "America/Toronto"}, 1800, winter) == {
+             dt(~N[2019-01-12 17:23:00]),
+             dt(~N[2019-01-19 16:38:48.907577])
+           }
+
+    # Winter time, later in the week:
+    assert Period.time_range({:weekly, :sun, "12:23", "America/Toronto"}, 1800, winter) == {
+             dt(~N[2019-01-13 17:23:00]),
+             dt(~N[2019-01-19 16:38:48.907577])
+           }
+  end
+
+  test "time_range/3 with {:weekly, day, time, zone} operates across DST boundaries" do
+    # Tuesday after EDT switchover:
+    after_edt = dt(~N[2019-03-12 16:38:48.907577])
+    # Friday after EST switchover:
+    after_est = dt(~N[2019-11-08 16:38:48.907577])
+
+    # Last Saturday at 09:23 EST (-4) = 14:23 UTC
+    assert Period.time_range({:weekly, :sat, "09:23", "America/Toronto"}, 1800, after_edt) == {
+             dt(~N[2019-03-09 14:23:00]),
+             after_edt
+           }
+
+    # Last Sunday (EDT day) at 09:23 EDT (-4) = 13:23 UTC
+    assert Period.time_range({:weekly, :sun, "09:23", "America/Toronto"}, 1800, after_edt) == {
+             dt(~N[2019-03-10 13:23:00]),
+             after_edt
+           }
+
+    # Last Sunday (EDT day) at 00:23 EST (-5) = 05:23 UTC
+    assert Period.time_range({:weekly, :sun, "00:23", "America/Toronto"}, 1800, after_edt) == {
+             dt(~N[2019-03-10 05:23:00]),
+             after_edt
+           }
+
+    # Last Saturday at 09:23 EDT (-4) = 13:23 UTC
+    assert Period.time_range({:weekly, :sat, "09:23", "America/Toronto"}, 1800, after_est) == {
+             dt(~N[2019-11-02 13:23:00]),
+             after_est
+           }
+
+    # Last Sunday (EST day) at 09:23 EST (-4) = 14:23 UTC
+    assert Period.time_range({:weekly, :sun, "09:23", "America/Toronto"}, 1800, after_est) == {
+             dt(~N[2019-11-03 14:23:00]),
+             after_est
+           }
+
+    # Last Sunday (EST day) at 00:23 EDT (-4) = 04:23 UTC
+    assert Period.time_range({:weekly, :sun, "00:23", "America/Toronto"}, 1800, after_est) == {
+             dt(~N[2019-11-03 04:23:00]),
+             after_est
+           }
+
+    # Last Sunday (EST day) at 01:23 EDT (-4) = 05:23 UTC
+    # This is during the "lost hour" of daylight savings.
+    # (There's currently no way to specify 01:23 EST.)
+    assert Period.time_range({:weekly, :sun, "01:23", "America/Toronto"}, 1800, after_est) == {
+             dt(~N[2019-11-03 05:23:00]),
+             after_est
            }
   end
 end
